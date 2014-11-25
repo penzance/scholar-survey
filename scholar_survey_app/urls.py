24bfe64ec42eda30035ea4e10de2ffa30315cdc3
Angela Ma <angelama@college.harvard.edu>
2014-11-25 14:20:20 -0500

    ScholarSurvey stuff

--- scholar_survey_app/urls.py
+++ scholar_survey_app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+# from scholar_survey_app.views import foo
 # from django.contrib import admin
 # admin.autodiscover()
 
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'scholar_survey_app/',include('scholar_survey_app.urls', namespace="scholar_survey_app")),
+    url(r'^$','scholar_survey_app.views.index', name='index'),
 )
--- scholar_survey_app/views.py
+++ scholar_survey_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import (render_to_response, render)
+from django.http import HttpResponse
 
+	#return HttpResponse("Hello, World!")
 # Create your views here.
+def index(request):
+	return render(request, 'scholar_survey_app/index.html', {
+		'message': 'This is Scholar Survey.',
+		})
